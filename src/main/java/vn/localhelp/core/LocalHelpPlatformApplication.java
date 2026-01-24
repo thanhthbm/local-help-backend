fc7c1b559b2677b60796ee6b6e350bec2672c7ee
Vu Minh Thanh <thanhthbm@gmail.com>
2026-01-24 13:31:41 +0700

    Initial commit

---
+++ src/main/java/vn/localhelp/core/LocalHelpPlatformApplication.java
@@ -0,0 +1,13 @@
+package vn.localhelp.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalHelpPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalHelpPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/localhelp/core/LocalHelpPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.localhelp.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalHelpPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
