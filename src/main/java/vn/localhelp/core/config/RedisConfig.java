a08068305e79b89a9626db15e039ceaadb69d169
Vu Minh Thanh <thanhthbm@gmail.com>
2026-03-30 15:57:54 +0700

    add caching support; implement Redis cache manager configuration and update CategoryResponse with constructors

--- src/main/java/vn/localhelp/core/config/RedisConfig.java
+++ src/main/java/vn/localhelp/core/config/RedisConfig.java
@@ -1,10 +1,14 @@
 package vn.localhelp.core.config;
 
+import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -26,5 +30,25 @@ public class RedisConfig {
     redisTemplate.afterPropertiesSet();
 
     return redisTemplate;
+  }
+
+  @Bean
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+    GenericJackson2JsonRedisSerializer serializer =
+        new GenericJackson2JsonRedisSerializer();
+
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                new StringRedisSerializer()))
+        .serializeValuesWith(
+            RedisSerializationContext.SerializationPair.fromSerializer(
+                serializer))
+        .entryTtl(Duration.ofMinutes(30));
+
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(config)
+        .build();
   }
 }
--- src/main/java/vn/localhelp/core/domain/response/category/CategoryResponse.java
+++ src/main/java/vn/localhelp/core/domain/response/category/CategoryResponse.java
@@ -1,10 +1,14 @@
 package vn.localhelp.core.domain.response.category;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryResponse {
   private Long id;
   private String name;
