b6d4c9a20d4e95c7d93b9a0fe569628be985698c
vuongptit <quocvuongdo123@gmail.com>
2026-04-14 12:01:53 +0700

    add api edit and setup profile

--- src/main/java/vn/localhelp/core/controller/UserController.java
+++ src/main/java/vn/localhelp/core/controller/UserController.java
@@ -1,21 +1,22 @@
 package vn.localhelp.core.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import vn.localhelp.core.domain.request.user.UpdateProfileRequest;
 import vn.localhelp.core.domain.response.user.UserResponse;
 import vn.localhelp.core.service.UserService;
 import vn.localhelp.core.util.FirebaseUtil;
 import vn.localhelp.core.util.annotation.ApiMessage;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import java.util.List;
 import vn.localhelp.core.domain.response.common.ResultPaginationDTO;
 import vn.localhelp.core.util.constant.UserStatus;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
@@ -23,10 +24,22 @@ public class UserController {
   private final UserService userService;
 
   @GetMapping("/me")
-  public ResponseEntity<UserResponse> getProfile(){
+  @ApiMessage("Lấy thông tin hồ sơ thành công")
+  public ResponseEntity<UserResponse> getProfile() {
     String currentUid = FirebaseUtil.getCurrentUserUid();
     return ResponseEntity.ok(userService.getMyProfile(currentUid));
   }
+
+  @PutMapping(value = "/me", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @ApiMessage("Cập nhật hồ sơ thành công")
+  public ResponseEntity<UserResponse> updateProfile(
+      @RequestPart(value = "data", required = false) @Valid UpdateProfileRequest request,
+      @RequestPart(value = "avatar", required = false) MultipartFile avatarFile
+  ) {
+    String currentUid = FirebaseUtil.getCurrentUserUid();
+    return ResponseEntity.ok(userService.updateProfile(currentUid, request, avatarFile));
+  }
+
   @GetMapping("/count-users")
   @ApiMessage("Lấy số lượng người dùng thành công")
   @PreAuthorize("hasRole('ADMIN')")
@@ -34,13 +47,14 @@ public class UserController {
     long count = userService.countTotalUsers();
     return ResponseEntity.ok(count);
   }
+
   @GetMapping("/admin/all")
   @PreAuthorize("hasRole('ADMIN')")
   @ApiMessage("Lấy danh sách người dùng cho Admin thành công")
   public ResponseEntity<ResultPaginationDTO<List<UserResponse>>> getAllUsers(
           @RequestParam(defaultValue = "1") int current,
           @RequestParam(defaultValue = "10") int pageSize
-  ){
+  ) {
     return ResponseEntity.ok(userService.getAllUsersForAdmin(current, pageSize));
   }
 
@@ -50,7 +64,7 @@ public class UserController {
   public ResponseEntity<UserResponse> updateUserStatus(
           @PathVariable Long id,
           @RequestParam UserStatus status
-  ){
+  ) {
     return ResponseEntity.ok(userService.updateUserStatus(id, status));
   }
 }
